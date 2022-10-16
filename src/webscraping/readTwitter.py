f9a3fd691f85fd8e7a474eb6db8993ac536362b1
wit4401 <wtownsend4401@gmail.com>
2022-10-15 20:13:42 -0400

    added metrics and relevancy tag

--- src/webscraping/readTwitter.py
+++ src/webscraping/readTwitter.py
@@ -4,11 +4,10 @@ import multiprocessing as mp
 import threading
 import pandas as pd
 
-
 def getTweets(client,search,iterate=1,results=100):
     tweetList = []
     for i in range(0,iterate,1):
-        response = client.search_recent_tweets(query=search,expansions='author_id',tweet_fields='created_at',max_results=results)
+        response = client.search_recent_tweets(query=search,expansions='author_id',tweet_fields=['created_at','public_metrics'],sort_order='relevancy',max_results=results)
     
         users = {u['id']: u for u in response.includes['users']}
         for info in response.data:
@@ -17,12 +16,14 @@ def getTweets(client,search,iterate=1,results=100):
                 user = users[info.author_id]            
                 tweetInfo.append(user.username) # adds username to result
             tweetInfo.append(info.text) # add content of tweet to result
+            tweetInfo.append(info.public_metrics['like_count'])
+            tweetInfo.append(info.public_metrics['retweet_count'])
             tweetInfo.append(info.created_at) # add when tweeted to result
             
             # adds complete "tuple" of information
             tweetList.append(tweetInfo)
 
-    data = pd.DataFrame(tweetList,columns=['User','Tweet','Date Tweeted'])
+    data = pd.DataFrame(tweetList,columns=['User','Tweet','# of likes','# of retweets','Date Tweeted'])
     return data
 
 if __name__ == "__main__":
@@ -30,30 +31,19 @@ if __name__ == "__main__":
     i = 1
     client = tweepy.Client(bearer_token=con.BEARER_TOKEN) # gives us access to the api in the program
 
-    # a list of queries to search for recent tweets
-    queries = ['#covid19 lang:en -is:retweet -has:media',
-                '#python lang:en -is:retweet -has:media',
-                '#c++ lang:en -is:retweet -has:media',
-                '#overwatch2 lang:en -is:retweet -has:media',
-                '#eldenring lang:en -is:retweet -has:media',
-                '#coding lang:en -is:retweet -has:media',
-                '#rickandmorty lang:en -is:retweet -has:media',
-                '#twitter lang:en -is:retweet -has:media',
-                '#salisburyuniversity lang:en -is:retweet -has:media',
-                '#metaquest lang:en -is:retweet -has:media',
-                '#southpark lang:en -is:retweet -has:media',
-                '#tiktoc lang:en -is:retweet -has:media',
-                '#breastcancerawareness lang:en -is:retweet -has:media',
-                '#iphone14 lang:en -is:retweet -has:media',
-                '#election2022 lang:en -is:retweet -has:media']
+    f = open("queries.txt","r")
+    queries=[]
+    for text in f:
+        queries.append(text)
     
     for query in queries:
-        print("Searching for tweets that fit the query: '{}'".format(queries[i-1]))
+        print("Searching for tweets that fit the query: {}".format(queries[i-1]),end='')
         # create a file name for the csv file
         fileName = "tweets" + str(i) + ".csv"
         i+=1 
-        data = getTweets(client,query,iterate=10) # grabs the most recent tweets
+        data = getTweets(client,query,results=10) # grabs the most recent tweets
         data.to_csv(fileName,index=False) # creates the csv file
         print("Done searching for those tweets.\n")
 
     print('Done.\nResults saved in *.csv files.')
+    f.close()
