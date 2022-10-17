d5f356ba53d5e5b97d8ad48e608701a3918fedce
jfernandez37 <jothomas0615@gmail.com>
2022-10-17 17:15:57 -0400

    Getting author name and date is more accurate. Also, the number of iterations per processor was turned down for presentation

--- src/webscraping/readGutenburg.py
+++ src/webscraping/readGutenburg.py
@@ -7,7 +7,7 @@ import multiprocessing as mp
 import threading
 from collections import Counter
 from string import punctuation
-numIter=100
+numIter=10
 URLBEGIN="https://www.gutenberg.org/cache/epub"
 def find_html(purl):
     page=urlopen(purl)
@@ -32,26 +32,49 @@ def get_title(htmlPage):
 
 def get_date(html_page):
     openT="Release Date:"
+    closedT="Language: "
     if html_page is None:
         return "DATE NOT FOUND"
-    elif openT not in html_page:
-        return"Copyright not found in page"
+    elif openT not in html_page or closedT not in html_page:
+        return"Date not found on page"
     else:
         index=html_page.find(openT)
         start=index+len(openT)
-        end=start+20
+        end=html_page.find(closedT)
         return html_page[start:end]
+
+
+def get_author(html_page):
+    openT="Author:"
+    closedT="Posting Date:"
+    closedT2="Release Date:"
+    if html_page is None:
+        return "AUTHOR NOT FOUND"
+    elif openT not in html_page:
+        return "Author not found in page"
+    else:
+        index=html_page.find(openT)
+        start=index+len(openT)
+        end=html_page.find(closedT)
+        end2=html_page.find(closedT2)
+        if closedT not in html_page and closedT2 not in html_page:
+            print("not in at all")
+        elif len(html_page[start:end])<len(html_page[start:end2]):
+            return html_page[start:end]
+        else:
+            return html_page[start:end2]
 
 
 def readWebpage(pageCount):
     print(pageCount)
     totalLen=0
     for i in range(numIter):
-        if (i%(numIter//10)==0 and i!=0):
+        '''if (i%(numIter//10)==0 and i!=0):
             tempPName=str(mp.current_process())
             tempPSplit=tempPName.split("ForkPoolWorker-")
             tempPSplit2=tempPSplit[1].split("\' parent=")
             print(str(i)+" iterations for processor: "+str(tempPSplit2[0])+" ========================================================================")
+        '''
         num=pageCount+i
         flag=0
         tempURL=URLBEGIN+"/"+str(num)+"/pg"+str(num)+".txt"
@@ -73,6 +96,8 @@ def readWebpage(pageCount):
                 print("Title too long")
             date=get_date(pageHtml)
             print(date)
+            author=get_author(pageHtml)
+            print(author)
         flag=0
     return totalLen
 
