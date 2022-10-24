be9334e18f7d626cc25e1a6c01e5088eb9fb1f64
wit4401 <wtownsend4401@gmail.com>
2022-10-24 13:16:18 -0400

    revised the file to work with the library

--- src/webscraping/readTwitter.py
+++ src/webscraping/readTwitter.py
@@ -1,31 +1,12 @@
 import tweepy
-import configTokens as con
+import tweet.readtweet as tw
 import multiprocessing as mp
 import threading
-import pandas as pd
-
-def getTweets(client,search,wanted_results=100):
-    tweetList = []
-    for response in tweepy.Paginator(client.search_recent_tweets,query=search,
-                                 expansions='author_id',tweet_fields=['created_at','public_metrics'],
-                                 sort_order='relevancy',max_results=100).flatten(limit=wanted_results):
-        tweetInfo = []
-        tweetInfo.append(response.text) # add content of tweet to result
-        tweetInfo.append(response.public_metrics['like_count']) # adds the like count
-        tweetInfo.append(response.public_metrics['retweet_count']) # adds the retweet count
-        tweetInfo.append(response.created_at) # add when tweeted to resul    
-        
-        # adds complete "tuple" of information
-        tweetList.append(tweetInfo)
-
-    print("Tweets Recieved: {}".format(len(tweetList)))
-    data = pd.DataFrame(tweetList,columns=['Tweet','# of likes','# of retweets','Date Tweeted'])
-    return data
 
 if __name__ == "__main__":
     print("Scraping Tweets...\n")
     i = 1
-    client = tweepy.Client(bearer_token=con.BEARER_TOKEN) # gives us access to the api in the program
+    client = tweepy.Client(bearer_token=tw.BEARER_TOKEN) # gives us access to the api in the program
 
     f = open("queries.md","r")
     queries=[]
@@ -37,7 +18,7 @@ if __name__ == "__main__":
         # create a file name for the csv file
         fileName = "tweets" + str(i) + ".csv"
         i+=1 
-        data = getTweets(client,query,1000) # grabs the most recent tweets
+        data = tw.getTweets(client,query,1000) # grabs the most recent tweets
         data.to_csv(fileName,index=False) # creates the csv file
         print("Done searching for those tweets.\n")
 
