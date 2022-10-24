f526ff1f7d25169165c509ca9d6381e7e933b46b
wit4401 <wtownsend4401@gmail.com>
2022-10-24 13:29:33 -0400

    merged the two tweet programs

--- src/webscraping/readTwitter.py
+++ src/webscraping/readTwitter.py
@@ -22,5 +22,15 @@ if __name__ == "__main__":
         data.to_csv(fileName,index=False) # creates the csv file
         print("Done searching for those tweets.\n")
 
-    print('Done.\nResults saved in *.csv files.')
+    print('Done.\nResults saved in *.csv files.\n')
     f.close()
+
+    users = ['elonmusk','WHO','NASA']
+    
+    for user in users:
+        userId = client.get_user(username=user)
+        print('Grabbing Tweets from @{}...'.format(user))
+        print('Tweets Received: {}'.format(tw.getUserTweets(client,user,userId.data.id)))
+        print('Finished grabbing tweets.\n')
+
+    print('Done.\nTweets saved to *.csv files.')
