71a4477bad960a1555ab4aeef553eb8bad656458
wit4401 <wtownsend4401@gmail.com>
2022-11-01 15:11:10 -0400

    put queries in a list instead of a file

--- src/webscraping/Twitter/readTwitter.py
+++ src/webscraping/Twitter/readTwitter.py
@@ -4,17 +4,27 @@ import multiprocessing as mp
 import threading
 
 if __name__ == "__main__":
+    client = tweepy.Client(bearer_token=tw.BEARER_TOKEN) # gives us access to the api in the program
+    queries = ['(death OR dead) lang:en -is:retweet -has:media -has:links',
+        '"Functional Programming" (#python OR #scala OR #haskell) lang:en -is:retweet -has:media -has:links',
+        '#c++ OR #c OR #csharp OR #java lang:en -is:retweet -has:media -has:links',
+        '#overwatch2 lang:en -is:retweet -has:media -has:links',
+        '"Salisbury University" lang:en -is:retweet -has:media -has:links',
+        '"computer science" lang:en -is:retweet -has:media -has:links',
+        '#rickandmorty lang:en -is:retweet -has:media -has:links',
+        '#youtube lang:en -is:retweet -has:media -has:links',
+        'college OR #collegelife lang:en -is:retweet -has:media -has:links',
+        '"Virtual Reality" OR VR lang:en -is:retweet -has:media -has:links',
+        '#southpark lang:en -is:retweet -has:media -has:links',
+        'Netflix #DAHMER lang:en -is:retweet -has:media -has:links',
+        'October (#halloween OR #breastcancerawareness) lang:en -is:retweet -has:media -has:links',
+        '#midterms2022 lang:en -is:retweet -has:media -has:links',
+        '#football OR #NFL lang:en -is:retweet -has:media -has:links']
+    
     print("Scraping Tweets...\n")
     i = 1
-    client = tweepy.Client(bearer_token=tw.BEARER_TOKEN) # gives us access to the api in the program
-
-    f = open("queries.md","r")
-    queries=[]
-    for text in f:
-        queries.append(text)
-    
     for query in queries:
-        print("Searching for tweets that fit the query: {}".format(queries[i-1]),end='')
+        print("Searching for tweets that fit the query: {}\n".format(queries[i-1]),end='')
         # create a file name for the csv file
         fileName = "tweets" + str(i) + ".csv"
         i+=1 
@@ -23,7 +33,6 @@ if __name__ == "__main__":
         print("Done searching for those tweets.\n")
 
     print('Done.\nResults saved in *.csv files.\n')
-    f.close()
 
     users = ['elonmusk','WHO','NASA']
     
