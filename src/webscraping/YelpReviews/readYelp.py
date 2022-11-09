66f82c9b8a49c981f08de9cb1962294b558e7db6
jcomfort1 <joshuacomfort@gmail.com>
2022-11-09 12:11:05 -0500

    Main method add to script

--- src/webscraping/YelpReviews/readYelp.py
+++ src/webscraping/YelpReviews/readYelp.py
@@ -8,4 +8,24 @@
 # Included libraries
 # ================================================================================
 
+# Method to pull from file (Will be implemented later)
 
+# Main method
+if __name__ =="__main__":
+    # Create the multithreading pool
+    pool=mp.Pool(mp.cpu_count())
+    
+    #Write the pages to the list
+    entryCount=[]
+    for i in range(mp.cpu_count()):
+        entryCount.append(20)
+    
+    #Print information to the console to inform the user on the number of threads available
+    print("Number of available processors: ", mp.cpu_count())
+
+    #Start threads
+    # pool.map(readWebpage, [pageNum for pageNum in entryCount])
+    
+    #Stop threads and write output to console
+    pool.close()
+    print("Done... pulled files written to MongoDB database")
