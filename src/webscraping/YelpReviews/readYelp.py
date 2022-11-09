a6be556a0129f44e49b74ada9d9a15672bffcc4b
jcomfort1 <joshuacomfort@gmail.com>
2022-11-09 12:07:59 -0500

    Added documentation to readYelp.py

---
+++ src/webscraping/YelpReviews/readYelp.py
@@ -0,0 +1,11 @@
+# Project Name(s): English Contextual Baseline Database
+# Program Name: readYelp.py
+# Date: 11/9/2022-...
+# Description: Read individual entries from the large dataset file
+# Saving format: List of data is written to the MongoDB database
+
+# ================================================================================
+# Included libraries
+# ================================================================================
+
+
