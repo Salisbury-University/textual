f7a5365f20e7148b7ee37c8ec96369d499eb653f
jcomfort1 <joshuacomfort@gmail.com>
2022-11-09 07:20:50 -0500

    Removed unused mongoDB file from webscraping directory

--- src/webscraping/mongoDB.py
+++
@@ -1,5 +0,0 @@
-# This file will store the code to access to mongoDB
-
-# Main function (does nothing right now)
-if __name__ == "__main__":
-    print("This will connect to the database in the future")
