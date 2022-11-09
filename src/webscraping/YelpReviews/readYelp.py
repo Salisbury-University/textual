630934ad5d4bc9932b71e05a6b8bcdae48fb8f80
jcomfort1 <joshuacomfort@gmail.com>
2022-11-09 16:56:21 -0500

    File is now read properly

--- src/webscraping/YelpReviews/readYelp.py
+++ src/webscraping/YelpReviews/readYelp.py
@@ -18,50 +18,91 @@ import json
 import numpy as np
 from collections import Counter
 
+# ================================================================================
+#                               DATABASE FUNTIONS
+# ================================================================================
+
+# Get authoriazation from file
+def get_credentials():
+    with open("mongopassword.txt", "r") as pass_file:
+        # Read each line from the file, splitting on newline
+        lines = pass_file.read().splitlines()
+    # Close the file and return the list of lines
+    pass_file.close()
+    return lines
+
+# Connect to the database
+def get_client():
+    # Needs to be done this way, can't push credentials to github
+    # Call the get pass function to open the file and extract the credentials
+    lines = get_credentials()
+
+    # Get the username from the file
+    username = lines[0]
+
+    # Get the password from the file
+    password = lines[1]
+    
+    # Set up a new client to the database
+    # Using database address and port number
+    client = MongoClient("mongodb://10.251.12.108:30000", username=username, password=password)
+
+    # Return the client
+    return client
+
+# Get the database, we are using the textual database | hardcoded currently (bad)
+def get_database(client):
+    return client.textual
+
+# Important, close the database
+def close_database(client):
+    # Close the connection to the database
+    client.close()
+
 # Open the file and return it
 def openFile(filename):
-    input_file = open(filename)
     return input_file
 
 # Method to pull from file (Will be implemented later)
-def pullReviews(input_str):
-    
-    print (input_str)
+def pullReviews(input_arr):
+    for line in input_arr:
+        print(line["review_id"])
+
     # Print current thread
     print("Thread: " + str(mp.current_process()))
 
 # Main method
 if __name__ =="__main__":
-    # Open the JSON file
-    input_file = openFile("yelp_academic_dataset_business.json")
-    
     # Holds review dictionaries
     reviews=[]
     
     # Parallelizing this part will increase the script's speed further
     # Iterate through the file and append the lines as dictionaries
-    for line in input_file:
-        # Load the line as JSON 
-        json_obj = json.loads(line)
-        # Append the JSON object to the end of the list
-        reviews.append(json_obj)
+    
+    # Open the JSON file
+    with open("yelp_academic_dataset_review_35000.json") as input_file: 
+        for line in input_file:
+            # Load the line as JSON 
+            json_obj = json.loads(line)
+            # Append the JSON object to the end of the list
+            reviews.append(json_obj)
 
     # Split larger array into n sub arrays where n is the number of available processors
     # This is done to allow each processor to process some of the data
     # Parallelizing this part greatly speeds up the process of writing the data to the database
-    arrays = np.array_split(reviews, mp.cpu_count())
+    reviewArray = np.array_split(reviews, mp.cpu_count())
 
     # Create the multithreading pool
     pool=mp.Pool(mp.cpu_count()) 
     
-    for array in arrays:
+    for array in reviewArray:
         print(len(array))
 
     #Print information to the console to inform the user on the number of threads available
     print("Number of available processors: ", mp.cpu_count())
 
     #Start threads
-    pool.map(pullReviews, [reviewEntry for reviewEntry in reviewList])
+    pool.map(pullReviews, [reviewSublist for reviewSublist in reviewArray])
     
     #Stop threads and write output to console
     pool.close()
