e0596e7cd1f234f96296ac83ff124bdc26b09feb
Zenny <joshuacomfort@gmail.com>
2022-11-12 19:23:10 -0500

    Changed DataConnect filename to DatabaseConnect and added proper documentation to the script

--- src/website/db_connect/DatabaseConnect.js
+++ src/website/db_connect/DatabaseConnect.js
@@ -1,16 +1,26 @@
+// Project Name(s): English Contextual Baseline Database
+// Program Name: DataConnect.js
+// Date: 11/10/2022-...
+// Description: Connects to the MongoDB database from the HS Linux server and displays DB data on a localhost
+
+//Import required packages
 var MongoClient = require('mongodb').MongoClient;
 var http = require('http')
 
+//Database url, does not contain the password for security purposes
 const url = "mongodb://10.251.12.108:30000?authSource=admin";
 
-var str = "";
 function connect_to_db(res) {
+	//Connect to the MongoDB
 	MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+		
+		//Get the db from MongoDB and search the RedditPost collection
 		var db = client.db("textual");
-
 		var cursor = db.collection('RedditPosts').find();
 
+		//For each item, append it to the HTML content
 		cursor.each(function(err, item) {
+			//Write until empty
 			if (item != null)
 				res.write("Post Title: " + item.title + "<br/>");
 			else
@@ -19,10 +29,12 @@ function connect_to_db(res) {
 	});
 }
 
+//Create local host server
 http.createServer(function (req, res) {
+	//Print header
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write("REDDIT POSTS<br/>");
+	//Connect to database and print data
 	connect_to_db(res);
 
-}).listen(8080);
-
+}).listen(8080); //Start the server
