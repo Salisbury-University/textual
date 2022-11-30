e0db8976a5bf9f41acdeeed949aecc1f7cb49f3f
cksmith21 <cksmith21@gmail.com>
2022-11-30 13:06:46 -0500

    need to test in database

--- src/analysis/PageAnalysis/LDAAnalysis.py
+++ src/analysis/PageAnalysis/LDAAnalysis.py
@@ -13,6 +13,45 @@ from nltk.stem.lancaster import LancasterStemmer
 from nltk.stem import WordNetLemmatizer
 import sys
 import os
+from pymongo import MongoClient
+
+# Get authoriazation from file
+def get_credentials():
+    with open("mongopassword.txt", "r") as pass_file:
+        # Read each line from the file, splitting on newline
+        lines = pass_file.read().splitlines()
+    # Close the file and return the list of lines
+    pass_file.close()
+    return lines
+
+# Connect to the database
+def get_client():
+    # Needs to be done this way, can't push credentials to github
+    # Call the get pass function to open the file and extract the credentials
+    lines = get_credentials()
+
+    # Get the username from the file
+    username = lines[0]
+
+    # Get the password from the file
+    password = lines[1]
+
+    # Set up a new client to the database
+    # Using database address and port number
+    client = MongoClient("mongodb://10.251.12.108:30000", username=username, password=password)
+    
+    # Return the client
+    return client
+
+# Get a database from the client
+# In this case use the textual database
+def get_database(client):
+    return client.textual
+
+# Close the connection to the database after data has been written
+def close_database(client):
+    # Close database connection
+    client.close()
 
 def lemmatize_stemming(text):
 
@@ -38,6 +77,8 @@ if __name__ == '__main__':
 
     # get documents 
 
+    # option one: get data from command line
+    '''
     if(len(sys.argv) < 2):
         print("Need path to documents.\n")
         sys.exit()
@@ -60,5 +101,18 @@ if __name__ == '__main__':
     results = get_dictionary(processed_docs)
 
     lda_model = gensim.models.LdaMulticore(results[0], num_topics = 8, id2word = results[1], passes = 10, workers = 2)
+    '''
 
-    
+    # option two: pull data from database???? 
+
+    client = get_client()
+    database = get_database(client)
+
+    collections = database.list_collection_names()
+
+    for col in collections:
+        print(col)
+
+
+
+
