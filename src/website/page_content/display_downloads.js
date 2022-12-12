1360d4de28ae590c4a075cfb3ec45e057f858737
Zenny00 <joshuacomfort@gmail.com>
2022-12-12 18:59:22 -0500

    Downloads page now provides button to download all the RedditPost data displayed, currently downloaded as giant string

--- src/website/page_content/display_downloads.js
+++ src/website/page_content/display_downloads.js
@@ -43,3 +43,23 @@ function load_downloads()
 		alert(error);
 	});
 }
+
+function downloadString() {	
+	fetch("/downloads", {method: "POST"}).then(data => data.text()).then((documents) => {
+	
+	var blob = new Blob([documents], { type: 'text/plain' });	
+
+	var a = document.createElement('a');
+	a.download = "fileName";
+	a.href = URL.createObjectURL(blob);
+	a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+	a.style.display = "none";
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+	setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
+
+	}).catch(function (error) {
+		alert(error);
+	});
+}
