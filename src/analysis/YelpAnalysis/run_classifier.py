69a3e1cebbba9596229b949e8fe9d548417d1166
Zenny00 <joshuacomfort@gmail.com>
2022-12-15 17:48:11 -0500

    Fixed issue where os suppression of TensorFlow warnings was not placed on a newline

--- src/analysis/YelpAnalysis/run_classifier.py
+++ src/analysis/YelpAnalysis/run_classifier.py
@@ -14,7 +14,8 @@
 # sklearn: data formatting
 # ================================================================================
 
-import os                                                                                                               os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Suppression of tensorflow warnings on script start
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Suppression of tensorflow warnings on script start
 from tensorflow import keras
 import sys
 from tensorflow.keras.preprocessing.sequence import pad_sequences
