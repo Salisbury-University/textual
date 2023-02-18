d66b8af0ef12116c5a05000d8fe03e1fb8b8ed25
jcomfort1 <joshuacomfort@gmail.com>
2023-02-18 15:43:43 -0500

    Added download functionality to the Yelp Review analysis tutorial page, users can now download the source code directly from the website. As of this commit issue #29 is now closed.

--- src/website/page_content/display_downloads.js
+++ src/website/page_content/display_downloads.js
@@ -74,3 +74,35 @@ function downloadData() {
 		alert(error);
 	});
 }
+
+// Download function for the neural network code
+function downloadNetworkCode(file, text) {	 
+	//creating an invisible element
+	var element = document.createElement('a');
+	element.setAttribute('href',
+	'data:text/plain;charset=utf-8, '
+	+ encodeURIComponent(text));
+	element.setAttribute('download', file);
+     
+	// Above code is equivalent to
+	// <a href="path of file" download="file name">
+     
+	document.body.appendChild(element);
+     
+	//onClick property
+	element.click();
+     
+	document.body.removeChild(element);
+    }
+     
+    // Start file download.
+    document.getElementById("btn")
+    .addEventListener("click", function() {
+	// Generate download of hello.txt
+	// file with some content
+	var text = document.getElementById("text").value;
+	var filename = "GFG.txt";
+     
+	download(filename, text);
+    }, false);	
+}
