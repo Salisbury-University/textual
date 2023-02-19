e59bade10d4d4501fc12d5d70f0117c6e85423d7
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2023-02-19 18:11:30 -0500

    Added yelp data to search

--- src/website/DatabaseConnect.js
+++ src/website/DatabaseConnect.js
@@ -65,3 +65,41 @@ app.post("/downloads", (req, res, next) => {
 		next(e)
 	}
 });
+app.post("/search", (req, res, next) => {
+	try
+	{
+		//Connect to the database
+		MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
+			assert.equal(null, err);
+			//Get the textual database
+			const db = client.db("textual");
+
+			//Create new promise
+			var myPromise = () => {
+				return new Promise((resolve, reject) => {
+					//Query the database and convert the result to an array
+					db.collection('YelpReviews').find().limit(10000).toArray(function(err, data) {
+						err ? reject(err) : resolve(data);
+					});
+				});
+			};
+
+			//Setup async call
+			var callMyPromise = async () => {
+				var result = await (myPromise());
+				return result;
+			};
+
+			callMyPromise().then(function(result) {
+				//Close the connection to the database client
+				client.close();
+				
+				//Send the query result to the client
+				res.send(result);
+			});
+		}); //End of MongoClient call
+
+	} catch (e) {
+		next(e)
+	}
+});
