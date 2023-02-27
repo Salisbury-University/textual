5d2851568fc7fac983895c6742f78685cd4beaad
imoki1 <92948511+imoki1@users.noreply.github.com>
2023-02-26 20:39:56 -0500

    Update SentimentAnalysisVADER.py

--- src/analysis/TextAnalysis/SentimentAnalysisVADER.py
+++ src/analysis/TextAnalysis/SentimentAnalysisVADER.py
@@ -1,3 +1,8 @@
+# SentimentAnalysisVADER.py
+# The program is sample tool that demonstrates how you can use the data in the textual
+# baseline database. The goal of this tool is to read 3 comments from a database and 
+# to determine whether these comments are positive, negative, or neutral.
+
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -6,22 +11,23 @@ pos = 0
 neg = 0
 neutral = 0
 
+# Read read the first 3 comments from the dataset.
 df = pd.read_csv("youtube_dataset.csv")
 comments = df['Comment'][:3]
 
+# Create SentimentIntensityAnalyzer object
 sia = SentimentIntensityAnalyzer()
 
 #This Loop goes through the first 3 comments in the dataset and gives them sentiment ratings.
-for i, comment in enumerate(comments, 1):
-
-    # Display the comment     
-    print("Comment " + str(i) + ":")
-    print(comment) 
+for i, comment in enumerate(comments, 1): 
     
     # polarity_scores is a method of SentimentIntensityAnalyzer
     # and returns a dictionary containing pos, neg, neu, and compound scores.
     sentiment_dict = sia.polarity_scores(comment)
     
+    print("Comment " + str(i) + ":")
+    print(comment)
+
     print("Overall sentiment dictionary is :", sentiment_dict)
     print("Sentence Overall Rated As ")
     
