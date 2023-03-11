568f2736ad01568a07754e7910716b43e239dd5e
imoki1 <92948511+imoki1@users.noreply.github.com>
2023-03-11 08:25:12 -0500

    Update CurrentTextAnalysis.py

--- src/analysis/TextAnalysis/CurrentTextAnalysis.py
+++ src/analysis/TextAnalysis/CurrentTextAnalysis.py
@@ -1,112 +1,49 @@
-#CurrentAnalysisFuncts.py
+# SentimentAnalysisVADER.py
+# The program is sample tool that demonstrates how you can use the data in the textual
+# baseline database. The goal of this tool is to read 3 comments from a database and 
+# to determine whether these comments are positive, negative, or neutral.
 
-import spacy
-#import re <-- might not be needed but leaving it here just incase
-import sklearn
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import time
-import threading
 
-# This Function creates a list of 3 tuples containing sentence, its determined sentiment, and the order it appears in text.
-def SentimentAnalysis(fullDoc):
+#Counters to keep track of positive, negative, and neutral sentiments
+pos = 0
+neg = 0
+neutral = 0
 
-    #separate our doc into a tuples of sentences
-    sentences = tuple(fullDoc.sents)
+# Read read the first 3 comments from the dataset.
+df = pd.read_csv("youtube_dataset.csv")
+comments = df['Comment'][:3]
 
-    # Create a SentimentIntensityAnalyzer object.
-    sid_obj = SentimentIntensityAnalyzer()
+# Create SentimentIntensityAnalyzer object
+sia = SentimentIntensityAnalyzer()
 
-    # Where we will store each tuple of data
-    sentiments = []
-    #This Loop does a sentence by sentence analysis of the text.
-    for index, sentence in enumerate(sentences, 1):
-
-        # polarity_scores is a method of SentimentIntensityAnalyzer
-        # object gives a sentiment dictionary.
-        # which contains pos, neg, neu, and compound scores.
-        
-        sentiment_dict = sid_obj.polarity_scores(sentence.text)
-        
-        # decide sentiment as positive, negative and neutral and count each sentence based on compound score.
-        if sentiment_dict['compound'] >= 0.05 :
-            result = "pos"
-            
-        elif sentiment_dict['compound'] <= - 0.05 :
-            result = "neg"
-            
-            
-        else :
-            result = "neu"
-
-        thisTuple = tuple((sentence.text, result, index))
-        sentiments.append(thisTuple)
+#This Loop goes through the first 3 comments in the dataset and gives them sentiment ratings.
+for i, comment in enumerate(comments, 1): 
     
-    print(sentiments)
-    return sentiments
-
-# This Function creates a list of 3 tuples containing words in the text, their part of speech and dependency tags
-def TagWords(fullDoc):
-
-    #separate our document into a list of sentences
-    sentences = tuple(fullDoc.sents)
-
-    # Where we will store each tuple of data
-    wordTags = []
-
-    # This loops through all sentences in the text.
-    for sentence in sentences:
-        
-        #This loop goes through every word in each sentence
-        #Create a 3 tuple for each token which contains each word, its dependency tag and its part of speech tag
-        for word in sentence:
-            thisTuple = tuple((word.text, word.dep_, word.pos_))
-            wordTags.append(thisTuple)
+    # polarity_scores is a method of SentimentIntensityAnalyzer
+    # and returns a dictionary containing pos, neg, neu, and compound scores.
+    sentiment_dict = sia.polarity_scores(comment)
     
-    print(wordTags)
-    return wordTags
+    print("Comment " + str(i) + ":")
+    print(comment)
 
-# This Function creates a list of 2 tuples containing of entities and their label (what type of entity they are)
-def getEntities(fullDoc):
+    print("Overall sentiment dictionary is :", sentiment_dict)
+    print("Sentence Overall Rated As ")
+    
+    # decide sentiment as positive, negative and neutral and count each sentence based on compound score.
+    if sentiment_dict['compound'] >= 0.05 :
+        print("Positive")
+        pos += 1
+        
+    elif sentiment_dict['compound'] <= - 0.05 :
+        print("Negative") 
+        neg += 1
+        
+    else :
+        print("Neutral")
+        neutral += 1
+    print()
 
-    #Where we will store each tuple of data
-    entities = []
-
-    for ent in fullDoc.ents:
-        thisTuple = tuple((ent.text, ent.label_))
-        entities.append(thisTuple)
-
-    print(entities)
-    return entities
-
-start_time = time.perf_counter() #This is to time the execution time
-
-with open("wikifull.txt", "r") as f:
-    text = f.read()
-
-#create the nlp object
-nlp = spacy.load("en_core_web_sm", exclude=["lemmatizer"])
-
-
-#add suffix rules to the tokenizer in our nlp object to seperate wikipedia in-text citations:
-suffixes = nlp.Defaults.suffixes + [r"[\.\,\?\!\:\;\'\"\}]"] + [r"\[\d+\]"]#[r"[\.\,\?\!\:\;\'\"\}]\[\d+\]"]
-suffix_regex = spacy.util.compile_suffix_regex(suffixes)
-nlp.tokenizer.suffix_search = suffix_regex.search
-
-#turn the text into a doc using the nlp
-fullDoc = nlp(text)
-
-pos = threading.Thread(target=TagWords, args=(fullDoc,))
-ner = threading.Thread(target=getEntities, args=(fullDoc,))
-senti = threading.Thread(target=SentimentAnalysis, args=(fullDoc,))
-
-senti.start()
-ner.start()
-pos.start()
-
-senti.join()
-ner.join()
-pos.join()
-
-
-print("--- %s seconds ---" % (time.perf_counter() - start_time))
+#display the number of sentences rated positive, negative, or neutral
+print("positive comments = ", pos, " negative comments = ", neg, " neutral comments = " , neutral)
