ca0fb280cb4f840846cd5af84bf4158c55f8883b
imoki1 <92948511+imoki1@users.noreply.github.com>
2023-03-11 08:26:53 -0500

    Update TextAnalysisForWebpage.py

--- src/analysis/TextAnalysis/TextAnalysisForWebpage.py
+++ src/analysis/TextAnalysis/TextAnalysisForWebpage.py
@@ -1,144 +1,49 @@
-#TextAnalysisForWebpage.py
+# SentimentAnalysisVADER.py
+# The program is sample tool that demonstrates how you can use the data in the textual
+# baseline database. The goal of this tool is to read 3 comments from a database and 
+# to determine whether these comments are positive, negative, or neutral.
 
-# This Program takes in one string and uses parses it using spaCy's nlp object to parse the text and turn it into a spaCy doc.
-# The doc object contains a list of tokens determined by spaCy's tokenizer. The Program reads a string of text read from a file of any length.
-# It creates a list of all recognized entities and their corresponding entity labels determined by spaCy's NER. The program goes through every token 
-# in each sentence creating a List of each token along with its dependency tag and part of speech tag determined by spaCy. It then analyzes the 
-# sentiment of each sentence using VADER's SentimentIntensityAnalyzer and rates the sentence as positive, negative, or neutral. 
-
-# Input:
-# string containing our text of interest (Currently only tested on wikipedia articles)
-
-# Result:
-# The program should create a list of tagged Entities, tagged Tokens(Words/Punctuations), and count the number of positive, 
-# neutral, and negative sentences.
-
-# Please note: All lines of code that are commented out in the form:
-"""
-<lines of code>
-"""
-# are for testing and debugging.
-
-import spacy
-import re
-import sklearn
 import pandas as pd
-from prettytable import PrettyTable
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import time
 
-
-
-
-#open and read in our text
-with open("wikifull.txt", "r") as f:
-    text = f.read()
-
-
-start_time = time.perf_counter()
-
-#create the nlp object
-nlp = spacy.load("en_core_web_sm")
-
-#add suffix rules to the tokenizer in our nlp object to seperate wikipedia in-text citations:
-suffixes = nlp.Defaults.suffixes + [r"[\.\,\?\!\:\;\'\"\}]"] + [r"\[\d+\]"]#[r"[\.\,\?\!\:\;\'\"\}]\[\d+\]"]
-suffix_regex = spacy.util.compile_suffix_regex(suffixes)
-nlp.tokenizer.suffix_search = suffix_regex.search
-
-#turn the text into a doc using the nlp
-fullDoc = nlp(text)
-
-
-#separate our document into a list of sentences
-sentences = tuple(fullDoc.sents)
-
-# Create a SentimentIntensityAnalyzer object.
-sid_obj = SentimentIntensityAnalyzer()
-
-# counters for the number of sentences determined to have
-# positive, negative, and neutral sentiments.
+#Counters to keep track of positive, negative, and neutral sentiments
 pos = 0
 neg = 0
 neutral = 0
 
-#List to store all tokens, their dependency tags, and pos tags
-WordTagList = []
+# Read read the first 3 comments from the dataset.
+df = pd.read_csv("youtube_dataset.csv")
+comments = df['Comment'][:3]
 
-#This Loop does a sentence by sentence analysis of the text.
-for sentence in sentences:
+# Create SentimentIntensityAnalyzer object
+sia = SentimentIntensityAnalyzer()
 
+#This Loop goes through the first 3 comments in the dataset and gives them sentiment ratings.
+for i, comment in enumerate(comments, 1): 
+    
     # polarity_scores is a method of SentimentIntensityAnalyzer
-    # object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
-     
-    print("Sentence:", sentence.text) 
+    # and returns a dictionary containing pos, neg, neu, and compound scores.
+    sentiment_dict = sia.polarity_scores(comment)
     
+    print("Comment " + str(i) + ":")
+    print(comment)
 
-    sentiment_dict = sid_obj.polarity_scores(sentence.text)
-    
     print("Overall sentiment dictionary is :", sentiment_dict)
-    #print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
-    #print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
-    #print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
-    print("Sentence Overall Rated As", end = " ")
+    print("Sentence Overall Rated As ")
     
     # decide sentiment as positive, negative and neutral and count each sentence based on compound score.
     if sentiment_dict['compound'] >= 0.05 :
-        
-        
         print("Positive")
-        
         pos += 1
+        
     elif sentiment_dict['compound'] <= - 0.05 :
-        
-        
-        print("Negative")
-        
+        print("Negative") 
         neg += 1
+        
     else :
-        
-        
         print("Neutral")
-        
         neutral += 1
-
-    #This loop goes through every word in each sentence
-    #Create a 3 tuple List of all tokens which contains each word, its dependency tag and its part of speech tag
-    for word in sentence:
-        WordTagList.append({"word": word.text, "dep": word.dep_, "pos": word.pos_})
-    
     print()
 
 #display the number of sentences rated positive, negative, or neutral
-print()
-print("positive sentences = ", pos, " negative sentences = ", neg, " neutral sentences = " , neutral)
-print()
-
-print()
-print("Named Entities:")
-print()
-
-
-# Create a list of 2 tuple entity objects consisting of entities and their label (what type of entity they are)
-# and adds them to a table to display the results
-entityList = []
-entityTable = PrettyTable(["Entity", "Label", "Label Meaning"])
-for ent in fullDoc.ents:
-    entityList.append({"entity": ent.text, "entityType": ent.label})
-    entityTable.add_row([ent.text, ent.label_, spacy.explain(ent.label_)])
-
-# Display the results if any named entities have been recognized
-if entityList == []:
-    print("Detected no named entities")
-else:
-    print(entityTable)
-
-print()
-print("Word with Tags:")
-print()
-
-#display all tokens and their given dependency and POS tags in a table:
-wordTable = PrettyTable(['Word', 'Dep', 'Pos'])
-for word in WordTagList:
-    wordTable.add_row([word["word"], word["dep"], word["pos"]])
-print(wordTable)
+print("positive comments = ", pos, " negative comments = ", neg, " neutral comments = " , neutral)
