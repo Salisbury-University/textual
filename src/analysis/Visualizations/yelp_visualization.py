8972951209f6bbf0523c5bce93b4b2fbb8bd2d8e
Zenny <joshuacomfort@gmail.com>
2023-03-11 21:28:54 -0500

    Initial commit to the data visualization branch

---
+++ src/analysis/Visualizations/yelp_visualization.py
@@ -0,0 +1,10 @@
+# Project Name(s): English Contextual Baseline Database
+# Program Name: yelp_visualization.py
+# Date: 03/11/2023
+# Description: Reads in the reviews.json file containing a large subset of the Yelp Review data dump and generates visual information explaining details regarding the dataset
+
+# ================================================================================
+# Included libraries
+# Pandas: storing data
+# JSON: reading json file
+# ================================================================================
