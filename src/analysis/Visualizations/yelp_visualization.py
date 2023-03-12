59b0962a0369756e1e67af205f710c8e7c148b9a
Zenny <joshuacomfort@gmail.com>
2023-03-11 22:38:59 -0500

    Print the 10 most common words for each subset of the reviews. Issue #47.

--- src/analysis/Visualizations/yelp_visualization.py
+++ src/analysis/Visualizations/yelp_visualization.py
@@ -10,6 +10,9 @@
 
 import pandas as pd
 import collections
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 if __name__ == "__main__":    
     # Load csv file, minimize memory usage using chunks
@@ -18,12 +21,16 @@ if __name__ == "__main__":
     # Five lists to hold the reviews based on the number of stars
     review_stars = [ [], [], [], [], [] ]
 
+    # 2D list to hold the most common words found in each review
+    review_words = [ [], [], [], [], [] ]
+
+    stop_words = set(stopwords.words("english"))
+
     # Iterate through the chunks
     for yelp_review_chunk in yelp_review_chunks:
 
         # Extract the review text based on the number of stars
         for index, row in yelp_review_chunk.iterrows():
-            print("appending")
             if (row["stars"] == 5.0):
                 review_stars[4].append(row["text"])
             elif (row["stars"] == 4.0):
@@ -35,6 +42,12 @@ if __name__ == "__main__":
             else:
                 review_stars[0].append(row["text"])
         
-
+    for review_type in review_stars: 
+        # Count the occurence of each word
+        num_words = collections.Counter(word for word in " ".join(review_type).split() if word.lower() not in stop_words)
+        most_common = num_words.most_common(10)
+        
+        for word, frequency in most_common:
+            print(f'{word}: {frequency}')
 
     print("Script done...")
