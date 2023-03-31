e6897294d5011e5451f9f0e84d3ef8eacd70256c
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2023-03-31 19:55:07 -0400

    Added code for showing results for the search function

--- src/website/DatabaseConnect.js
+++ src/website/DatabaseConnect.js
@@ -24,7 +24,7 @@ app.use(express.static(__dirname + "/page_content"));
 app.use(bodyParser.urlencoded({extended: true}));
 //Start the app on port 8080
 app.listen(8080);
-
+const searchResults=[];
 //The function will be called when the user clicks on the downloads page
 //Data will be posted, and can be fetched by the client to be displayed on the downloads page
 app.post("/downloads", (req, res, next) => {
@@ -66,46 +66,6 @@ app.post("/downloads", (req, res, next) => {
 		next(e)
 	}
 });
-app.post("/search", (req, res, next) => {
-	try
-	{
-		//Connect to the database
-		MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
-			assert.equal(null, err);
-			//Get the textual database
-			const db = client.db("textual");
-
-			//Create new promise
-			var myPromise = () => {
-				return new Promise((resolve, reject) => {
-					//Query the database and convert the result to an array
-					db.collection('YelpReviews').find().limit(10000).toArray(function(err, data) {
-						err ? reject(err) : resolve(data);
-					});
-				});
-			};
-
-			//Setup async call
-			var callMyPromise = async () => {
-				var result = await (myPromise());
-				return result;
-			};
-
-			callMyPromise().then(function(result) {
-				//Close the connection to the database client
-				client.close();
-				
-				//Send the query result to the client
-				res.send(result);
-			});
-		}); //End of MongoClient call
-
-	} catch (e) {
-		next(e)
-	}
-});
-
-
 app.get('/search2', function(req, res) {
 	res.render('search2.html');
 })
@@ -120,18 +80,20 @@ app.post('/search2', function(req, res) {
                         assert.equal(null, err);
                         //Get the textual database
                         const db = client.db("textual");
-
                         //Create new promise
                         var myPromise = () => {
                                 return new Promise((resolve, reject) => {
+					console.log(searchResults);
                                         //Query the database and convert the result to an array
                                         db.collection('YelpReviews').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataYelp) {
 						db.collection('RedditPosts').find({selftext:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataReddit) {
 							db.collection('YoutubeComment').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataYoutubeComments){
 								err ? reject(err) : resolve(dataYelp.concat(dataReddit.concat(dataYoutubeComments)));
+								//searchResults.concat(dataYelp.concat(dataReddit.concat(dataYoutubeComments)));
 							});
 						});
-                                        });	
+                                        });
+					//err ? reject(err) : resolve(searchResults);
                                 });
                         };
 
@@ -143,13 +105,25 @@ app.post('/search2', function(req, res) {
 
                         callMyPromise().then(function(result) {
                                 //Close the connection to the database client
-                                client.close();
+				const firstHalf = fs.readFileSync(__dirname + "/search_results.html", "utf-8");
+				firstHalf.split(/\r?\n/).forEach(line=> {
+					res.write(line);
+				});
+				for (const textElement of result) {
+					res.write("<tr><td>"+textElement.text+"</td></tr>");
+				}
+				const secondHalf = fs.readFileSync(__dirname + "/search_results2.html","utf8");
+				secondHalf.split(/\r?\n/).forEach(line=> {
+                                        res.write(line);
+                                });
+				client.close();
 				res.send(result);
+				//res.send(301, { Location: "http://localhost:8080/search2.html" });
 				//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
 
-                        });
-                }); //End of MongoClient call
-		//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
+                        	});
+                	}); //End of MongoClient call
+			//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
 
         } catch (e) {
                 next(e)
