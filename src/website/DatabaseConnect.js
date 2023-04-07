03f82ffda520259d1aec25d38174b4a419dd0cd1
Joe Fernandez <52220317+jfernandez37@users.noreply.github.com>
2023-04-07 16:37:29 -0400

    Updated to allow the user to select which collection they want to search through. Also limited the output

--- src/website/DatabaseConnect.js
+++ src/website/DatabaseConnect.js
@@ -24,7 +24,6 @@ app.use(express.static(__dirname + "/page_content"));
 app.use(bodyParser.urlencoded({extended: true}));
 //Start the app on port 8080
 app.listen(8080);
-const searchResults=[];
 //The function will be called when the user clicks on the downloads page
 //Data will be posted, and can be fetched by the client to be displayed on the downloads page
 app.post("/downloads", (req, res, next) => {
@@ -71,7 +70,6 @@ app.get('/search2', function(req, res) {
 })
 
 app.post('/search2', function(req, res) {
-	console.log(req.body.searchTerm);
 	//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
     	try
         {
@@ -80,19 +78,113 @@ app.post('/search2', function(req, res) {
                         assert.equal(null, err);
                         //Get the textual database
                         const db = client.db("textual");
+			/*db.listCollections().toArray(function(err, data) {
+				let i =0;
+				while (i<data.length){
+					console.log(data[i]);
+					i++;
+				}
+			});*/
                         //Create new promise
                         var myPromise = () => {
                                 return new Promise((resolve, reject) => {
-					console.log(searchResults);
-                                        //Query the database and convert the result to an array
-                                        db.collection('YelpReviews').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataYelp) {
-						db.collection('RedditPosts').find({selftext:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataReddit) {
+					collections=[]
+					if(typeof req.body.yelpCB!=='undefined'){
+						collections.push('YelpReviews');
+					}
+					if(typeof req.body.redditCB!=='undefined'){
+						collections.push('RedditPosts');
+					}
+					if(typeof req.body.youtubeCB!=='undefined'){
+						collections.push('YoutubeComment');
+					}
+					if(typeof req.body.twitterCB!=='undefined'){
+						collections.push('TwitterTweets');
+					}
+					if(typeof req.body.pgCB!=='undefined'){
+						collections.push('PGText');
+					}
+					if(typeof req.body.wsCB!=='undefined'){
+						collections.push('WikiSourceText');
+					}
+					if(typeof req.body.RC2CB!=='undefined'){
+						collections.push('RedditComments_v2')
+					}
+					if(typeof req.body.amazonCB!=='undefined'){
+						collections.push('AmazonReviews');
+					}
+					                                        //Query the database and convert the result to an array
+                                        /*db.collection('YelpReviews').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataYelp) {
+						db.collection('RedditPosts').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataReddit) {
 							db.collection('YoutubeComment').find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err, dataYoutubeComments){
 								err ? reject(err) : resolve(dataYelp.concat(dataReddit.concat(dataYoutubeComments)));
 								//searchResults.concat(dataYelp.concat(dataReddit.concat(dataYoutubeComments)));
 							});
 						});
-                                        });
+                                        });*/
+					if(collections.length>0){
+					db.collection(collections[0]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes1){
+						if(collections.length>1){
+						db.collection(collections[1]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes2){
+							if(collections.length>2){
+							db.collection(collections[2]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes3){
+								if(collections.length>3){
+								db.collection(collections[3]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes4){
+									if(collections.length>4){
+									db.collection(collections[4]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes5){
+										if(collections.length>5){
+										db.collection(collections[5]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes6){
+											if(collections.length>6){
+											db.collection(collections[6]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes7){
+												if(collections.length>7){
+												db.collection(collections[7]).find({text:{'$regex' : req.body.searchTerm, '$options' : 'i'}}).limit(3).toArray(function(err,sRes8){
+													if(collections.length>8){
+														console.log("uh oh");
+													}
+													else{
+														err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3.concat(sRes4.concat(sRes5.concat(sRes6.concat(sRes7.concat(sRes8))))))));
+													}
+												});
+												}
+												else{
+														err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3.concat(sRes4.concat(sRes5.concat(sRes6.concat(sRes7)))))));
+												}
+											});
+											}
+											else{
+													err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3.concat(sRes4.concat(sRes5.concat(sRes6))))));
+											}
+										});					
+									}
+										else{
+												err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3.concat(sRes4.concat(sRes5)))));
+										}
+									});
+									}
+									else{
+											err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3.concat(sRes4))));
+									}
+								});
+								}
+								else{
+									err ? reject(err) : resolve(sRes1.concat(sRes2.concat(sRes3)));
+								}
+							});
+							}
+							else{
+								err ? reject(err) : resolve(sRes1.concat(sRes2));
+							}
+						});
+						}
+						else{
+							err ? reject(err) : resolve(sRes1);
+						}
+					});
+					}
+					else{
+						err ? reject(err) : resolve([]);
+					}
+
 					//err ? reject(err) : resolve(searchResults);
                                 });
                         };
@@ -110,20 +202,15 @@ app.post('/search2', function(req, res) {
 					res.write(line);
 				});
 				for (const textElement of result) {
-					res.write("<tr><td>"+textElement.text+"</td></tr>");
+					res.write("<tr><td>"+textElement.text.substring(0,1000)+"</td></tr>");
 				}
 				const secondHalf = fs.readFileSync(__dirname + "/search_results2.html","utf8");
 				secondHalf.split(/\r?\n/).forEach(line=> {
                                         res.write(line);
                                 });
 				client.close();
-				res.send(result);
-				//res.send(301, { Location: "http://localhost:8080/search2.html" });
-				//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
-
                         	});
                 	}); //End of MongoClient call
-			//res.writeHead(301, { Location: "http://localhost:8080/search_results.html" });
 
         } catch (e) {
                 next(e)
