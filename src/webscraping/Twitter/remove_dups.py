c378082c9fba96a32d8086c737f5b88fb9c52971
wit4401 <wtownsend4401@gmail.com>
2023-04-09 13:07:00 -0400

    duplicate scripts for YouTube, Twitter, Reddit

---
+++ src/webscraping/Twitter/remove_dups.py
@@ -0,0 +1,55 @@
+import pymongo
+
+# Get authoriazation from file
+def get_credentials():
+    with open("mongopassword.txt", "r") as pass_file:
+        # Read each line from the file, splitting on newline
+        lines = pass_file.read().splitlines()
+    # Close the file and return the list of lines
+    pass_file.close()
+    return lines
+
+# Connect to the database
+def get_client():
+    # Needs to be done this way, can't push credentials to github
+    # Call the get pass function to open the file and extract the credentials
+    lines = get_credentials()
+
+    # Get the username from the file
+    username = lines[0]
+
+    # Get the password from the file
+    password = lines[1]
+    
+    # Set up a new client to the database
+    # Using database address and port number
+    client = pymongo.MongoClient("mongodb://10.251.12.108:30000", username=username, password=password)
+
+    # Return the client
+    return client
+
+# Get the database, we are using the textual database | hardcoded currently (bad)
+def get_database(client):
+    return client.textual
+
+# Important, close the database
+def close_database(client):
+    # Close the connection to the database
+    client.close()
+
+if __name__=="__main__":
+    client=get_client()
+    db = get_database(client)
+    cur = db.TwitterTweets.aggregate(
+        [
+            {"$group":{"_id":"$text","unique_ids":{"$addToSet":"$_id"},"count":{"$sum":1}}},
+            {"$match":{"count":{"$gte":2}}}
+        ]
+    )
+    dup_ids =[]
+    for data in cur:
+        del data["unique_ids"][0]
+        for data_id in data["unique_ids"]:
+            dup_ids.append(data_id)
+    collection.remove({"_id":{"$in":dup_ids}})
+    close_database(client)
