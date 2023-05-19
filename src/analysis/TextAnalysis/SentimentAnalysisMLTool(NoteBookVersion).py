565ff1855680aa701f702e417632d67fe26eb8b6
imoki1 <92948511+imoki1@users.noreply.github.com>
2023-05-18 23:30:26 -0400

    Finalize SentimentAnalysisMLTool(NoteBookVersion).py

--- src/analysis/TextAnalysis/SentimentAnalysisMLTool(NoteBookVersion).py
+++ src/analysis/TextAnalysis/SentimentAnalysisMLTool(NoteBookVersion).py
@@ -1,7 +1,11 @@
 # SentimentAnalysisML(NotebookVersion).py
+# This is the sample tool for demonstrating Sentiment Analysis using a
+# machine learning approach.
+# 
 
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
 from nltk.corpus import stopwords 
 from nltk.corpus import wordnet
 from nltk import word_tokenize
@@ -10,12 +14,25 @@ from nltk import pos_tag
 import string
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer #, TfidfVectorizer 
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from langdetect import detect 
 from mlxtend.plotting import plot_confusion_matrix
 
+# <--------------------------------------------------------------------->
+# This function takes a string as input. (In this case a YouTube Comment)
+# 
+# Returns the string "en" if the detected language is english
+# Returns the string "not_en" if the detected language is some non-english language
+# 
+# If an exception is raised, it is likely because no language could be detected.
+# This mostly occurs in comments that are garbage or containing only punctuations or symbols
+# so this is used to filter out noisy comments.
+# 
+# Note: This function may sometimes misclassifies the language so there may still be non-english or noisy comments even after
+# this is called. However the number of noisy or non-english comments should significantly decrease.
+# <--------------------------------------------------------------------->
 def detect_english(str):
     try:
         if detect(str) == "en":
@@ -25,6 +42,13 @@ def detect_english(str):
     except:
         return "not_en"
 
+# <--------------------------------------------------------------------->
+# This function takes a string as input. (In this case a treebank part of speech (pos) tag)
+#
+# This function is used to convert treebank pos tags into the wordnet pos tag format. since the lemmatizer used in 
+# the 'remove_stopwords()' function requires the wordnet format to work, this function is crucial.
+# The converted tag is returned.
+# <--------------------------------------------------------------------->
 def get_wordnet_pos(treebank_tag):
 
     if treebank_tag.startswith('J'):
@@ -38,13 +62,22 @@ def get_wordnet_pos(treebank_tag):
     else:
         return wordnet.NOUN
 
+# <--------------------------------------------------------------------->
+# This function takes a string as input. (In this case a YouTube Comment)
+#
+# This function does more than just removing stopwords despite the name of the function (Name of the function can be modified if needed)
+
+# This function does some of the most important preprocessing steps such as stopword removal, lemmatization, and removing punctuation.
+# 
+# Returns a string that has been lemmatized, removed of stopwords, and removed of punctuation 
+# <--------------------------------------------------------------------->
 def remove_stopwords(line):
     # remove stop words to keep only the words that add meaning to the sentence
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(line)
     lemmatizer = WordNetLemmatizer()
 
-    # Lemmetize each meaningful word to make more useful observations
+    # Lemmetize each token that isn't a stopword to make more useful observations
     filtered_sentence = [lemmatizer.lemmatize(w, get_wordnet_pos(tag)) for w, tag in pos_tag(word_tokens) if not w in stop_words]
     
     # get rid of articles and left over contraction tokens
@@ -52,16 +85,36 @@ def remove_stopwords(line):
         if len(w) < 2:
             filtered_sentence.remove(w)
 
+    # Join remaining tokens together
     str = " ".join(filtered_sentence)
 
+    # Create a string of punctuations we would like to remove
     puncts = string.punctuation
+
+    # Exclude '?', '!', and '.' from removal since they add some context for sentiment of sentences.
     my_puncts = puncts.replace("?","").replace("!","").replace(".","")
 
+    # delete every punctuation except '?', '!', and '.'
     for punct in my_puncts:
         str = str.replace(punct, "")
 
     return str
 
+# <--------------------------------------------------------------------->
+# This function takes a pandas dataframe as input. (In this case the dataframe containing the YouTube Comments)
+# 
+# This function applies all the preprocessing steps defined above and more to each row of the dataframe.
+# 
+# Here is the complete list of steps for preprocessing:
+# 
+# 1. Filter out non-english and noisy/garbage comments
+# 2. Apply VADER's polarity_scores() function to each comment to create a new column called "Polarity".
+# So that we can get a general idea of the sentiment of each comment.
+# 3. Apply remove_stopwords() function defined above to create a new column called "stopwords_removed".
+# 4. Shuffle the dataset randomly to prepare the dataset for the testing and training split.
+# 
+# Returns the resulting dataframe after performing all operations.
+# <--------------------------------------------------------------------->
 def preprocess(df):
 
     # Use detect_english function to tag english comments and non-english comments
@@ -71,24 +124,15 @@ def preprocess(df):
 
     # Evaluate sentiment of each comment
 
+    # Create VADER SentimentIntensityAnalyzer object
+    sia = SentimentIntensityAnalyzer()
+
     # Apply VADER's polarity_scores() function to each element
     df["Polarity"] = df["Comment"].map(lambda x: sia.polarity_scores(x)['compound'])
 
-    # Shuffle the rows
-    df.sample(frac=1).reset_index(drop=True)
-
-    # Define Categories based on compound polarity score
-    # -1: Negative
-    # 0: Neutral
-    # 1: Positive
-
-    df['Categorization'] = 0
-    df.loc[df['Polarity'] > 0.05, 'Categorization'] = 1
-    df.loc[df['Polarity'] < -0.05, 'Categorization'] = -1
-
-    #Download nltk tokenizer and stopwords
-    #nltk.download("punkt")
-    #nltk.download("stopwords")
+    # Download nltk tokenizer and stopwords
+    nltk.download("punkt")
+    nltk.download("stopwords")
 
     # convert all letters to lowercase:
     df['Comment'] = df['Comment'].map(lambda x: x.lower())
@@ -99,7 +143,11 @@ def preprocess(df):
     # Evaluate sentiment of each comment.
     # Apply VADER's polarity_scores() function to each element of a column in parallel
 
+    # Apply the remove_stopwords function to remove stopwords and lemmatize each comment
     df["stopwords_removed"] = df['Comment'].map(remove_stopwords)
+
+        # Shuffle the rows
+    df.sample(frac=1).reset_index(drop=True)
 
     return df
 
@@ -107,21 +155,28 @@ if __name__ == '__main__':
 
     pd.options.mode.chained_assignment = None
 
-    # Create SentimentIntensityAnalyzer object
-    sia = SentimentIntensityAnalyzer()
-
-    # Read read the first 3 comments from the dataset.
+    # Read the comments from the dataset.
     df = pd.read_csv("youtube_dataset.csv", usecols=["Comment"])
 
     # Preprocess the data
     print("Preprocessing...")
     df = preprocess(df)
+
+    # Define Categories based on compound polarity score
+    # -1: Negative
+    # 0: Neutral
+    # 1: Positive
+    df['Categorization'] = 0
+    df.loc[df['Polarity'] > 0.05, 'Categorization'] = 1
+    df.loc[df['Polarity'] < -0.05, 'Categorization'] = -1
     
-    # count the frequency of each class
+    # Plot the distribution of sentiments that have been determined by VADER:
+    print("Showing the distribution of sentiments that were determined by VADER")
     counts = df['Categorization'].value_counts()
+
+    # Create a list of labels and counts for each respective class (negative, neutral, and positive)
     frequencies = [counts[-1], counts[0], counts[1]]
 
-    # Create labels for the figure
     labels = ["negative", "neutral", "positive"]
 
     # Display Sentiment Distribution in the dataset
@@ -146,15 +201,14 @@ if __name__ == '__main__':
     tf_train = vect.fit_transform(X_train)
     tf_test = vect.transform(X_test)
 
-    print("\nvect vocabulary: ", vect.vocabulary_)
-
-    lr = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=300)
+    # Create Multinomial Logistic Regression Model (Since dealing with multiple classes)
+    lr = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=250)
     lr.fit(tf_train,y_train)
-    
-    # Check accuracy score on both datasets
+
+    # Print accuracy of self and independent tests.
     print("Checking Accuracy of score on both datasets")
-    print("Accuracy score on training dataset:",lr.score(tf_train,y_train))
-    print("Accuracy score on test dataset:", lr.score(tf_test,y_test))
+    print("Accuracy score on training dataset:",round(lr.score(tf_train,y_train), 2))
+    print("Accuracy score on test dataset:", round(lr.score(tf_test,y_test), 2))
     print()
 
     # Make predictions on the test dataset
@@ -162,30 +216,28 @@ if __name__ == '__main__':
     expected = y_test
     predicted = lr.predict(tf_test)
 
-    # Create a log file to manually see what is happening
-    index_list = expected.index.tolist()
-    with open("log.txt", "w", encoding="utf-8") as file:
-        for i, index in enumerate(index_list):
-            file.write("Comment: " + df.loc[index, 'Comment'] + "\nstopwords removed: " +  df.loc[index, 'stopwords_removed'] + "\nexpected:" + str(expected.values.flatten()[i]) + "\npredicted:" + str(predicted[i]) + "\n\n")
-    
-    #Plot Confusion matrix for the test dataset
+    # Plot Confusion matrix for the test dataset
     print("Plotting confusion matrix for the test dataset...")
+    #Plot and Display confusion matrix for the test dataset
+    print("Plotting and displaying confusion matrix for the independent test...")
     cf = metrics.confusion_matrix(expected,predicted,labels = [1, 0, -1])
     print(cf)
+    print()
     fig, ax = plot_confusion_matrix(conf_mat = cf, class_names = [1, 0, -1])
     plt.show()
 
-    print("Showing Classification report...")
+    print("Showing Classification report:")
     # Print classification report
     print(metrics.classification_report(expected, predicted))
-
-    print("Showing F1 Score.")
+    
     # Find F1 Score
-    print(metrics.f1_score(expected, predicted, average='macro'))
+    print("Showing F1 Score:")
+    print(round(metrics.f1_score(expected, predicted, average='macro'), 2))
+    print()
 
-    # If you would like to test the model your own comments
+    # To test your own comments on the model
     while True:
-        entry = input("Enter a YouTube comment: ")
+        entry = input("Enter a YouTube comment for sentiment classification: ")
 
         #Lowercase the input and get rid of whitespace
         entry = entry.lower().strip()
